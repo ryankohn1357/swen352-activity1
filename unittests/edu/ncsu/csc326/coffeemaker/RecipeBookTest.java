c8d302f42e19c7a8fcaf358b0c6e09a90cdb9fc3
Ryan Kohn <ryankohn@ryans-mbp-6.wireless.rit.edu>
2020-01-28 18:29:12 -0500

    added empty test classes

--- unittests/edu/ncsu/csc326/coffeemaker/RecipeBookTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/RecipeBookTest.java
@@ -2,7 +2,7 @@ package edu.ncsu.csc326.coffeemaker;
 
 import junit.framework.TestCase;
 
-public class InventoryTest extends TestCase {
+public class RecipeBookTest extends TestCase {
 
 	protected void setUp() throws Exception {
 		super.setUp();
